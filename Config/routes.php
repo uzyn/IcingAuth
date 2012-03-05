4e9fb6cfbbe6cbe6123a58b6ad615a7adc190273
U-Zyn Chua <chua@uzyn.com>
2012-03-05 14:15:44 +0800

    Bad routing rules fixed

--- Config/routes.php
+++ Config/routes.php
@@ -2,6 +2,6 @@
 /**
  * Routing alias for IcingAuth
  */
-	Router::connect('/icingauth/:controller/*', array('plugin' => 'IcingAuth'));
 	Router::connect('/icingauth/:controller/:action/*', array('plugin' => 'IcingAuth'));
+	Router::connect('/icingauth/:controller/*', array('plugin' => 'IcingAuth'));
 	
